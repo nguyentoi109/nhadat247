c3f0212732ba3d0194a0a8ab2220af365ff31de9
nguyentoi <nguyentoiv109@gmail.com>
2026-04-27 18:46:47 +0700

    fix loi duong dan

--- wp-content/themes/haphome/pages/bds-chothue-biet-thu-binh-phuoc.php
+++ wp-content/themes/haphome/pages/bds-chothue-biet-thu-binh-phuoc.php
@@ -7,7 +7,7 @@ get_header();
 <section class="container wrap-content">
 	<main role="main" class="full-page">
 		<div class="list-style">
-			<?php get_template_part('loop-property/binh-phuoc/loop-chothue-biethtu'); ?>
+			<?php get_template_part('loop-property/binh-phuoc/loop-chothue-bietthu'); ?>
 		</div>
 	</main>
 </section>
