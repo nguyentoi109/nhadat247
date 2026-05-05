c4a14991f22849165a371f2748b4f042e2e10562
nguyentoi <nguyentoiv109@gmail.com>
2026-05-05 22:17:56 +0700

    fix size item

--- wp-content/themes/haphome/pagination.php
+++ wp-content/themes/haphome/pagination.php
@@ -1,10 +1,5 @@
-<?php
-global $wp_query;
-
-echo paginate_links(array(
-    'total' => $wp_query->max_num_pages,
-    'current' => max(1, get_query_var('paged')),
-    'prev_text' => '« Trước',
-    'next_text' => 'Sau »'
-));
-?>
+<!-- pagination -->
+<div class="pagination">
+	<?php html5wp_pagination(); ?>
+</div>
+<!-- /pagination -->
